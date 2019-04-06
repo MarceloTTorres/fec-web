393dff6c8074ba48a42e855799a5cce46a989cf6
Igor Rocha <igoor01@hotmail.com>
2019-04-06 02:50:01 -0300

    change user registration view

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'ra' => ['required', 'string', 'min:8', 'max:8', 'unique:users']
         ]);
     }
 
@@ -66,7 +67,9 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'ra' => $data['ra'],
             'password' => Hash::make($data['password']),
+            'photo' => $data['photo'],
         ]);
     }
 }
