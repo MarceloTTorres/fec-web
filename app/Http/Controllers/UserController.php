97bad693e0fb225e8893a6bac5f067b86501eac0
Eduardo Luiz Femia <elfemia@uniara.edu.br>
2019-04-15 13:44:36 -0300

    cria metodo de alteração do usuario parcialmente
    pendencia de alteração da imagem de usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Carbon\Carbon;
 
 class UserController extends Controller
 {
-    //
+    public function AlterUser(Request $request){
+        $campos = $request->all();
+        $user = User::find($campos->id);
+        $user->fill($campos);
+
+        // $nome = Carbon::now()->format('Y_m_d_h_i_s') . 
+        //     '.' .$campos->file('imagem')->getClientOriginalExtensions();
+
+        // $campos->imagem->move(public_path('images\\' . $campos->id), $nome);
+         
+        $user->save();
+    }
 }
