664bc3051eef4bb8617757453405e48e1dab65f4
elfemia <48606932+elfemia@users.noreply.github.com>
2019-05-03 19:54:57 -0300

    implementa validação de e-mail na alteração

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,16 @@ class UserController extends Controller
         $user = User::find($request->id);  
         $user->fill($request->all());
 
+        $rules['email'] = ['email|required', 'regex:/^[\w.]+@[(uniara)]+\.[(edu)]+\.([(br)]+)?$/i'];
+
+        $request->validate([
+          'name' => 'required'
+      ]);
+
+        //'^[a-z0-9.]+@[(uniara)]+\.[(edu)]+\.([(br)]+)?$/i'
+        
+      
+
         //verifica se tem foto
 
         //se tiver, apaga a foto atual do disco -- da update no nome da nova foto
